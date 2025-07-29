3f79f56197eead28f8a879c70e96890a167f8305
Velislav Shopov <shopov.velislav@gmail.com>
2025-07-29 12:07:39 +0300

    added password field

--- UserAPI/Models/User.cs
+++ UserAPI/Models/User.cs
@@ -1,11 +1,20 @@
-﻿namespace UserAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scalefocus_HobbyAPI_Database.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; }
+
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string Email { get; set; }
+
     }
 }
