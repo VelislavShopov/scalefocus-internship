1afb50c4fc24cc29b5b02074dd5f9e9d2b278b03
Velislav Shopov <shopov.velislav@gmail.com>
2025-08-07 19:13:37 +0300

    connection string back to the main db

--- UserAPI/Models/PasswordResetToken.cs
+++ UserAPI/Models/PasswordResetToken.cs
@@ -8,7 +8,7 @@ namespace UserAPI.Models
         public int Id { get; set; }
         public Guid UserId { get; set; }
         public string? PasswordResetTokenValue { get; set; }
-        public DateTime? PasswordResetTokenExpires { get; set; }
+        public DateTime? ExpiryTime { get; set; }
 
         public User User { get; set; } = null!;
     }
